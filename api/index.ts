ecce3e190b1fc11808ef69be0a4529f1a7a13490
Rodeobazaar124 <azfasa15@gmail.com>
2023-08-24 12:59:55 +0700

    hmm

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { logger } from "../src/app/logging";
 const fs = require("fs");
 const fsP = require("fs/promises");
-export const publicdir = path.join(__dirname, "..", "public");
+export const publicdir = path.join(__dirname, "public");
 export const checkOrCreateFolder = function (folder: string | any) {
   if (!fs.existsSync(folder)) {
     fsP.mkdir(path.join(folder));
